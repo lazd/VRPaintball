5e7288d1924550d724d9c0f49027008648f76eda
Larry Davis <lazdnet@gmail.com>
2017-05-30 00:07:00 -0700

    Splatoonity!

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -12,7 +12,7 @@ public class Bullet : MonoBehaviour
 
     public float destroyTime = 0f;
 
-    public GameObject PaintSplatter;
+    public float splatScale = 1.0f;
 
     void OnCollisionEnter(Collision collision)
     {
@@ -39,9 +39,8 @@ public class Bullet : MonoBehaviour
             // Draw a paint splatter
             // Todo: don't draw if hit another bullet
             var contact = collision.contacts[0];
-            var splatter = Instantiate(PaintSplatter, contact.point, Quaternion.FromToRotation(Vector3.up, contact.normal));
-            splatter.transform.parent = collision.gameObject.transform;
-        
+            createSplat(contact);
+
             if (hitObject.CompareTag("Bullet"))
             {
                 // Destroy immediately if bullets hit
@@ -56,5 +55,54 @@ public class Bullet : MonoBehaviour
         }
 
         hasHit = true;
+    }
+
+    void createSplat(ContactPoint hit) {
+        // Get how many splats are in the splat atlas
+        int splatsX = SplatManagerSystem.instance.splatsX;
+        int splatsY = SplatManagerSystem.instance.splatsY;
+        Vector4 channelMask = new Vector4(1,0,0,0);
+
+        // if( Input.GetKeyDown (KeyCode.Alpha1) ){
+            // channelMask = new Vector4(1,0,0,0);
+        // }
+        
+        // if( Input.GetKeyDown (KeyCode.Alpha2) ){
+            channelMask = new Vector4(0,1,0,0);
+        // }
+        
+        // if( Input.GetKeyDown (KeyCode.Alpha3) ){
+        //     channelMask = new Vector4(0,0,1,0);
+        // }
+        
+        // if( Input.GetKeyDown (KeyCode.Alpha4) ){
+        //     channelMask = new Vector4(0,0,0,1);
+        // }
+
+        Vector3 leftVec = Vector3.Cross ( hit.normal, Vector3.up );
+        float randScale = Random.Range(0.5f,1.5f);
+        
+        GameObject newSplatObject = new GameObject();
+        newSplatObject.transform.position = hit.point;
+        if( leftVec.magnitude > 0.001f ){
+            newSplatObject.transform.rotation = Quaternion.LookRotation( leftVec, hit.normal );
+        }
+        newSplatObject.transform.RotateAround( hit.point, hit.normal, Random.Range(-180, 180 ) );
+        newSplatObject.transform.localScale = new Vector3( randScale, randScale * 0.5f, randScale ) * splatScale;
+
+        Splat newSplat;
+        newSplat.splatMatrix = newSplatObject.transform.worldToLocalMatrix;
+        newSplat.channelMask = channelMask;
+
+        float splatscaleX = 1.0f / splatsX;
+        float splatscaleY = 1.0f / splatsY;
+        float splatsBiasX = Mathf.Floor( Random.Range(0,splatsX * 0.99f) ) / splatsX;
+        float splatsBiasY = Mathf.Floor( Random.Range(0,splatsY * 0.99f) ) / splatsY;
+
+        newSplat.scaleBias = new Vector4(splatscaleX, splatscaleY, splatsBiasX, splatsBiasY );
+
+        SplatManagerSystem.instance.AddSplat (newSplat);
+
+        GameObject.Destroy( newSplatObject );
     }
 }
