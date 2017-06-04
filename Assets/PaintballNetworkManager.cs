02a2ae1c518c3b3f8464a0682be82282141dd8c1
Larry Davis <lazdnet@gmail.com>
2017-06-04 03:40:37 -0700

    Loop colors when out of colors, add green

--- Assets/PaintballNetworkManager.cs
+++ Assets/PaintballNetworkManager.cs
@@ -7,8 +7,9 @@ public class PaintballNetworkManager : NetworkManager {
     private Color[] playerColors = new Color[] {
         new Color(0.9f, 0f, 0), // Red
         new Color(1, 0.55f, 0), // Orange
-        new Color(1, 0, 0.85f), // Pink
-        new Color(0, 0.50f, 1) // Blue
+        new Color(0, 0.9f, 0), // Green
+        new Color(0, 0.50f, 1), // Blue
+        new Color(1, 0, 0.85f) // Pink
     };
     private int lastColor = 0;
 
@@ -17,9 +18,23 @@ public class PaintballNetworkManager : NetworkManager {
         base.OnServerAddPlayer(conn, playerControllerId);
         if (conn.playerControllers.Count > 0)
         {
+            if (lastColor == playerColors.Length-1)
+            {
+                // Wrap around
+                lastColor = 0;
+            }
+
             GameObject player = conn.playerControllers[0].gameObject;
             player.GetComponent<PlayerController>().color = playerColors[lastColor];
             lastColor++;
         }
+    }
+
+    public void Update()
+    {
+        // Find all spheres
+        // Increment score
+        // Reset round if time > round time
+
     }
 }
